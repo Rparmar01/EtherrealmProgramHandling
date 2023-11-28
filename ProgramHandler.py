9af495dcb41b05cbd2fa9621e8d6be6cf6a0ce39
Rparmar01 <rparmar.us@gmail.com>
2023-11-28 13:45:40 -0500

    Autosaves generated output to Meshfiles

--- ProgramHandler.py
+++ ProgramHandler.py
@@ -1,15 +1,26 @@
-from json import load
 import os 
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
 
-# Run text2pointcloud notebook to create a pointcloud from text and convert pointcloud into a ply mesh
-createPCloud = "Point-eAutomatedFlow/point-e/point_e/examples/text2pointcloudANDpointcloud2mesh.ipynb"
-with open(createPCloud) as fp:
-    nb = load(fp)
+# PREREQUISITE: nbconvert must be installed and all prerequisites of shap-e notebook must be installed before running.
 
-for cell in nb['cells']:
-    if cell['cell_type'] == 'code':
-        source = ''.join(line for line in cell['source'] if not line.startswith('%'))
-        exec(source, globals(), locals())
+# Specify the path to your Jupyter Notebook file
+notebook_path = ''
+
+# Read the notebook
+with open(notebook_path) as f:
+    notebook = nbformat.read(f, as_version=4)
+
+# Create an ExecutePreprocessor. The timeout=-1 means no timeout for cell execution, but this can be adjusted
+execute_processor = ExecutePreprocessor(timeout=-1)
+
+# Execute the notebook
+execute_processor.preprocess(notebook, {'metadata': {'path': '.'}})
+
+# Save modified notebooks (Will preserve updated output within notebook)
+with open(notebook_path, 'w', encoding='utf-8') as f:
+    nbformat.write(notebook, f)
+
 
 # Run ply to fbx conversion script to bake ply mesh to fbx object file
 os.system("python ply_to_fbx.py")
