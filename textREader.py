f1b81f07702a2688bb4605a611199e5e3e79af76
Ritik Parmar <35667956+Rparmar01@users.noreply.github.com>
2023-12-06 15:34:27 -0500

    Debugging file reading errors

--- textREader.py
+++ textREader.py
@@ -1,14 +1,14 @@
 import time
 from watchdog.observers import Observer
-from watchdog.events import PatternMatchingEventHandler
+from watchdog.events import FileSystemEventHandler
 
-class MyHandler(PatternMatchingEventHandler):
-    patterns = ["*.txt"]  # Adjust the pattern based on your file type
-
+class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        super().on_modified(event)
-        print(f'File {event.src_path} has been modified.')
-        read_and_print_updated_text(event.src_path)
+        if event.is_directory:
+            return
+        elif event.event_type == 'modified':
+            print(f'File {event.src_path} has been modified.')
+            read_and_print_updated_text(event.src_path)
 
 def read_and_print_updated_text(file_path):
     with open(file_path, 'r') as file:
@@ -16,8 +16,9 @@ def read_and_print_updated_text(file_path):
         print(f'Updated Text:\n{updated_text}')
 
 if __name__ == "__main__":
-    file_path_to_watch = f"C:\\Users\\rparm\\Documents\\GitHub\\Shap-eTextToMeshGit\\EtherrealmProgramHandling\\sample_text_input.txt"
- 
+    file_path_to_watch = f"C:\\Users\\rparm\\Documents\\GitHub\\Shap-eTextToMeshGit\\EtherrealmProgramHandling\\sample_text_input.txt"  # Windows path
+    # file_path_to_watch = "/absolute/path/to/your/text/file.txt"  # Linux path
+
     event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, path=file_path_to_watch, recursive=False)
